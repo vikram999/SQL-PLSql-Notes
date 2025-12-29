58492b050466bc8a808c30bbc56a6dbd805de69e
vikram kumar <vikram.zenith999@gmail.com>
2025-12-29 16:20:56 +0530

    Update copy_noCopy_out_parameters.sql

--- copy_noCopy_out_parameters.sql
+++ copy_noCopy_out_parameters.sql
@@ -1,3 +1,31 @@
+--NO copy performs better in case of large volume of data is being IN/OUT from a Function 
+--It does not pass that actual value ,instead it passes the reference/address where it is kept in memory 
+--so it avoids overhead of keeping backandForth 
+
+--in case of OUT as CLOB BLOB LONG , it performs better 
+-- if it is smaller cases , then it won't make any sense and benefit 
+--it is request to Oracle , not an order , so oracle may ignore this 
+
+--This has side effects also 
+--if in between some exception raises then it does not keeps original value passed 
+--in case of IN OUT param , it messes the values , few will be before changes few will be changed once 
+--Rollback won't make any difference , as that works on database level not on memory variables.
+--Below is the example that shows the messed values in case any error occurs 
+--one more example is there without NOCOPY , that keeps original values in case of any execeptions 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 CREATE OR REPLACE PROCEDURE p_test_nocopy (
    pit_array IN OUT NOCOPY lt_associated_array
 ) AS
